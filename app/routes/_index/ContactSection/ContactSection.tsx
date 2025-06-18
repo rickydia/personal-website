7c51e80bdd46d46888115e5e150dc7d5ac113e18
ricky <rikitikitang@gmail.com>
2025-06-18 13:40:34 -0700

    feat: adds contact me section

---
+++ app/routes/_index/ContactSection/ContactSection.tsx
@@ -0,0 +1,36 @@
+import { RiLinkedinBoxLine, RiMailLine } from "@remixicon/react";
+
+import { AppLink, Typography, TypographyVariant } from "~/components";
+
+import css from "./styles.module.css";
+
+export function ContactSection() {
+  return (
+    <section className={css.container}>
+      <Typography variant={TypographyVariant.HEADING}>
+        Let's get in touch!
+      </Typography>
+
+      <div className={css.content}>
+        <AppLink to="mailto:rickydg@proton.me" className={css.link}>
+          <RiMailLine className={css.icon} />
+          <Typography variant={TypographyVariant.SUBHEADING}>
+            Email Me
+          </Typography>
+        </AppLink>
+        <Typography variant={TypographyVariant.DEFAULT_BOLD} className={css.or}>
+          or
+        </Typography>
+        <AppLink
+          to="https://www.linkedin.com/in/rickydg"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={css.link}
+        >
+          <RiLinkedinBoxLine className={css.icon} />
+          <Typography variant={TypographyVariant.SUBHEADING}>DM Me</Typography>
+        </AppLink>
+      </div>
+    </section>
+  );
+}
